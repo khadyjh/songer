bd2eb468b2173cbe2a9f28dcf7ed6209614c3187
khadyjh <khadyjh2015@gmail.com>
2022-03-27 10:57:43 +0300

    lab14

---
+++ Bcrypt/src/test/java/com/example/Bcrypt/BcryptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Bcrypt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BcryptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
